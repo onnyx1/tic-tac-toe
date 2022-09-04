8be3796fd40182579a47e849cc448dcdc673ab45
onnyx1 <spencer.retcher1@gmail.com>
2022-09-04 19:45:32 -0400

    Added Event Listeners to Interface.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const gameBoard = (() => {
   };
   const setBoard = function (array) {
     board = array;
+    display();
   };
 
   const display = function () {
@@ -24,11 +25,6 @@ const gameBoard = (() => {
   return { getBoard, setBoard, changeElement };
 })();
 
-
-
-
-
-
 const player = (name) => {
   const getName = () => {
     return name;
@@ -40,11 +36,6 @@ const player = (name) => {
 
   return { getName, move };
 };
-
-
-
-
-
 
 const displayController = (function () {
   const x_player = player("x");
@@ -68,539 +59,347 @@ const displayController = (function () {
     }
   };
 
-
-  const winCondition = function() {
-
-
-    const winCondition = [[gameBoard.getBoard()[0], gameBoard.getBoard()[1], gameBoard.getBoard()[2]], [gameBoard.getBoard()[3], gameBoard.getBoard()[4], gameBoard.getBoard()[5]],
-    [gameBoard.getBoard()[6], gameBoard.getBoard()[7], gameBoard.getBoard()[8]], [gameBoard.getBoard()[0], gameBoard.getBoard()[3], gameBoard.getBoard()[6]],
-    [gameBoard.getBoard()[1], gameBoard.getBoard()[4], gameBoard.getBoard()[7]], [gameBoard.getBoard()[2], gameBoard.getBoard()[5], gameBoard.getBoard()[8]],
-    [gameBoard.getBoard()[0], gameBoard.getBoard()[4], gameBoard.getBoard()[8]], [gameBoard.getBoard()[2], gameBoard.getBoard()[4], gameBoard.getBoard()[6]]];
+  const winCondition = function () {
+    const winCondition = [
+      [gameBoard.getBoard()[0], gameBoard.getBoard()[1], gameBoard.getBoard()[2]],
+      [gameBoard.getBoard()[3], gameBoard.getBoard()[4], gameBoard.getBoard()[5]],
+      [gameBoard.getBoard()[6], gameBoard.getBoard()[7], gameBoard.getBoard()[8]],
+      [gameBoard.getBoard()[0], gameBoard.getBoard()[3], gameBoard.getBoard()[6]],
+      [gameBoard.getBoard()[1], gameBoard.getBoard()[4], gameBoard.getBoard()[7]],
+      [gameBoard.getBoard()[2], gameBoard.getBoard()[5], gameBoard.getBoard()[8]],
+      [gameBoard.getBoard()[0], gameBoard.getBoard()[4], gameBoard.getBoard()[8]],
+      [gameBoard.getBoard()[2], gameBoard.getBoard()[4], gameBoard.getBoard()[6]],
+    ];
 
     let winX;
     let winO;
     for (let row = 0; row < winCondition.length; row++) {
-        winX = 0;
-        winO = 0;
-        for (let column = 0; column < winCondition[row].length; column++) {
-
-            if (winCondition[row][column] === ("x")) {
-                winX++;
-            }
-
-            if (winCondition[row][column] === ("o")) {
-                winO++;
-            }
-
-            if (winX === 3) {
-                console.log("X Wins");
-            }
-
-            if (winO === 3) {
-              console.log("O Wins")
-            }
-
+      winX = 0;
+      winO = 0;
+      for (let column = 0; column < winCondition[row].length; column++) {
+        if (winCondition[row][column] === "x") {
+          winX++;
         }
+
+        if (winCondition[row][column] === "o") {
+          winO++;
+        }
+
+        if (winX === 3) {
+          console.log("X Wins");
+        }
+
+        if (winO === 3) {
+          console.log("O Wins");
+        }
+      }
     }
 
     console.log("no one wins");
-  }
+  };
 
-
-
-
-  const heuristic_function = function(board) {
-
-
+  const heuristic_function = function (board) {
     if (board[0] == board[1] && board[1] == board[2]) {
-
-      if (board[0]===("x")) {
-          return +5;
+      if (board[0] === "x") {
+        return +5;
       }
 
-      if (board[0]===("o")) {
-          return -5;
+      if (board[0] === "o") {
+        return -5;
       }
-
     }
 
-      if (board[3] == board[4] && board[4] == board[5]) {
-
-        if (board[3]===("x")) {
-            return +5;
-        }
-
-        if (board[3]===("o")) {
-            return -5;
-        }
-
+    if (board[3] == board[4] && board[4] == board[5]) {
+      if (board[3] === "x") {
+        return +5;
       }
 
-        if (board[6] == board[7] && board[7] == board[8]) {
-
-          if (board[6]===("x")) {
-              return +5;
-          }
-
-          if (board[6]===("o")) {
-              return -5;
-          }
-
-        }
-
-
-
-        if (board[0] == board[3] && board[3] == board[6]) {
-
-          if (board[0]===("x")) {
-              return +5;
-          }
-
-          if (board[0]===("o")) {
-              return -5;
-          }
+      if (board[3] === "o") {
+        return -5;
       }
-
-
-      if (board[1] == board[4] && board[4] == board[7]) {
-
-        if (board[1]===("x")) {
-            return +5;
-        }
-
-        if (board[1]===("o")) {
-            return -5;
-        }
     }
 
+    if (board[6] == board[7] && board[7] == board[8]) {
+      if (board[6] === "x") {
+        return +5;
+      }
+
+      if (board[6] === "o") {
+        return -5;
+      }
+    }
+
+    if (board[0] == board[3] && board[3] == board[6]) {
+      if (board[0] === "x") {
+        return +5;
+      }
+
+      if (board[0] === "o") {
+        return -5;
+      }
+    }
+
+    if (board[1] == board[4] && board[4] == board[7]) {
+      if (board[1] === "x") {
+        return +5;
+      }
+
+      if (board[1] === "o") {
+        return -5;
+      }
+    }
 
     if (board[2] == board[5] && board[5] == board[8]) {
-
-      if (board[2]===("x")) {
-          return +5;
+      if (board[2] === "x") {
+        return +5;
       }
 
-      if (board[2]===("o")) {
-          return -5;
+      if (board[2] === "o") {
+        return -5;
       }
-  }
-
+    }
 
     if (board[0] == board[4] && board[4] == board[8]) {
+      if (board[0] === "x") {
+        return +5;
+      }
 
-        if (board[0]===("x")) {
-            return +5;
-        }
-
-        if (board[0]===("o")) {
-            return -5;
-        }
+      if (board[0] === "o") {
+        return -5;
+      }
     }
 
     if (board[2] == board[4] && board[4] == board[6]) {
+      if (board[2] === "x") {
+        return +5;
+      }
 
-        if (board[2]===("x")) {
-            return +5;
-        }
-
-        if (board[2]===("o")) {
-            return -5;
-        }
+      if (board[2] === "o") {
+        return -5;
+      }
     }
 
     return 0;
+  };
 
-}
-
-
-
-
-
-
-
-const movesRemaining = function(board) {
-
-    for(let i = 0; i < 9; i++) {
-            if (board[i] == "") {
-                return true;
-            }
+  const movesRemaining = function (board) {
+    for (let i = 0; i < 9; i++) {
+      if (board[i] == "") {
+        return true;
+      }
     }
 
     return false;
+  };
 
-}
+  const testDraw = function (board) {
+    let draw = 0;
 
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "x" || board[i] === "o") {
+        draw++;
+      }
+    }
 
+    if (draw == 9) {
+      return "Draw";
+    } else {
+      return "Playing";
+    }
+  };
 
-
-const miniMax = function(board, depth, isMaximizer) {
-
-  let outcome = heuristic_function(board);
-  if (outcome === 5) {
+  const miniMax = function (board, depth, isMaximizer) {
+    let outcome = heuristic_function(board);
+    if (outcome === 5) {
       return outcome;
-  }
+    }
 
-  if (outcome === -5) {
+    if (outcome === -5) {
       return outcome;
-  }
+    }
 
-  if (movesRemaining(board) === false) {
+    if (movesRemaining(board) === false) {
       return 0;
-  }
+    }
 
-  if (isMaximizer === true) {
-
+    if (isMaximizer === true) {
       let bestValue = -10000000;
 
       for (let i = 0; i < 9; i++) {
+        if (board[i] === "") {
+          board[i] = "x";
 
-              if (board[i] === "") {
+          bestValue = Math.max(bestValue, miniMax(board, depth + 1, !isMaximizer));
 
-                  board[i] = "x";
-
-                  bestValue = Math.max(bestValue, miniMax(board, depth + 1, !isMaximizer));
-
-                  board[i] = "";
-
-              }
-          
+          board[i] = "";
+        }
       }
 
       return bestValue;
-
-  } else {
-
+    } else {
       let bestValue = 1000;
 
       for (let j = 0; j < 9; j++) {
+        if (board[j] === "") {
+          board[j] = "o";
 
-              if (board[j]==="") {
+          bestValue = Math.min(bestValue, miniMax(board, depth + 1, !isMaximizer));
 
-                  board[j] = "o";
-
-                  bestValue = Math.min(bestValue, miniMax(board, depth + 1, !isMaximizer));
-
-                  board[j] = "";
-
-              }
-          
+          board[j] = "";
+        }
       }
       return bestValue;
+    }
+  };
 
-  }
+  const optimalMove = function (board) {
+    let bestValue = 10000;
 
-}
+    let coords = -1;
 
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = "o";
 
+        let value = miniMax(board, 0, true);
+        board[i] = "";
 
-
-
-const optimalMove = function(board) {
-
-  let bestValue = -10000;
-
-  let coords = -1;
-
-  for (let i = 0; i < 9; i++) {
-
-          if (board[i] === "") {
-              board[i] = "x";
-
-              let value = miniMax(board, 0, false);
-            console.log("minimax" + value);
-              board[i] = "";
-
-              if (value > bestValue) {
-                  coords = i;
-                  bestValue = value;
-
-              }
-
-          }
+        if (value < bestValue) {
+          coords = i;
+          bestValue = value;
+        }
       }
-  
-  return coords;
-}
+    }
 
+    return coords;
+  };
 
+  const optimalMove2 = function (board) {
+    let bestValue = -10000;
 
+    let coords = -1;
 
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = "x";
 
+        let value = miniMax(board, 0, false);
+        board[i] = "";
 
+        if (value > bestValue) {
+          coords = i;
+          bestValue = value;
+        }
+      }
+    }
 
+    return coords;
+  };
 
+  const eraseEvent = function () {
+    const buttons = document.querySelectorAll(".grid button");
+    for (let button of buttons) {
+      button.replaceWith(button.cloneNode(true));
+    }
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
   const buttons = document.querySelectorAll(".grid button");
   for (let button of buttons) {
     button.addEventListener("click", function () {
       if (validSpot(button)) {
-        o_player.move(button.getAttribute("data-num"));
-        x_player.move(optimalMove(gameBoard.getBoard()));
-
+        x_player.move(button.getAttribute("data-num"));
+        winCondition();
+        if (testDraw(gameBoard.getBoard()) === "Draw") {
+          alert("Draw!");
+        }
+        o_player.move(optimalMove(gameBoard.getBoard()));
+        winCondition();
+        if (testDraw(gameBoard.getBoard()) === "Draw") {
+          alert("Draw!");
+        }
       }
     });
   }
 
-return {movesRemaining, heuristic_function, optimalMove};
 
+  const x = document.querySelector(".x");
+  x.classList.add("style-button");
+  const o = document.querySelector(".o");
+
+  x.addEventListener("click", () => {
+    x.classList.add("style-button");
+    o.classList.remove("style-button");
+
+    gameBoard.setBoard(["", "", "", "", "", "", "", "", ""]);
+
+    eraseEvent();
+
+    const buttons = document.querySelectorAll(".grid button");
+    for (let button of buttons) {
+      button.addEventListener("click", function () {
+        if (validSpot(button)) {
+          x_player.move(button.getAttribute("data-num"));
+          winCondition();
+          if (testDraw(gameBoard.getBoard()) === "Draw") {
+            alert("Draw!");
+          }
+          o_player.move(optimalMove(gameBoard.getBoard()));
+          winCondition();
+          if (testDraw(gameBoard.getBoard()) === "Draw") {
+            alert("Draw!");
+          }
+        }
+      });
+    }
+  });
+
+  o.addEventListener("click", () => {
+    o.classList.add("style-button");
+    x.classList.remove("style-button");
+
+    gameBoard.setBoard(["", "", "", "", "", "", "", "", ""]);
+
+    eraseEvent();
+
+    x_player.move(Math.floor(Math.random() * 9));
+
+    const buttons = document.querySelectorAll(".grid button");
+    for (let button of buttons) {
+      button.addEventListener("click", function () {
+        if (validSpot(button)) {
+          o_player.move(button.getAttribute("data-num"));
+          winCondition();
+          if (testDraw(gameBoard.getBoard()) === "Draw") {
+            alert("Draw!");
+          }
+
+          x_player.move(optimalMove2(gameBoard.getBoard()));
+
+          winCondition();
+
+          if (testDraw(gameBoard.getBoard()) === "Draw") {
+            alert("Draw!");
+          }
+        }
+      });
+    }
+  });
+
+
+  const restart = document.querySelector(".restart");
+
+  restart.addEventListener("click",()=>{
+
+    gameBoard.setBoard(["", "", "", "", "", "", "", "", ""]);
+
+    if(o.classList.contains("style-button")){
+      x_player.move(Math.floor(Math.random() * 9));
+
+    }
+
+
+  })
+
+
+  return { movesRemaining, heuristic_function, optimalMove };
 })();
