77477289515d7e87a61d1b61636f5c5e17255e08
onnyx1 <spencer.retcher1@gmail.com>
2022-09-04 17:49:57 -0400

    Finished Basic AI.

--- js/script.js
+++ js/script.js
@@ -109,16 +109,498 @@ const displayController = (function () {
 
 
 
+  const heuristic_function = function(board) {
+
+
+    if (board[0] == board[1] && board[1] == board[2]) {
+
+      if (board[0]===("x")) {
+          return +5;
+      }
+
+      if (board[0]===("o")) {
+          return -5;
+      }
+
+    }
+
+      if (board[3] == board[4] && board[4] == board[5]) {
+
+        if (board[3]===("x")) {
+            return +5;
+        }
+
+        if (board[3]===("o")) {
+            return -5;
+        }
+
+      }
+
+        if (board[6] == board[7] && board[7] == board[8]) {
+
+          if (board[6]===("x")) {
+              return +5;
+          }
+
+          if (board[6]===("o")) {
+              return -5;
+          }
+
+        }
+
+
+
+        if (board[0] == board[3] && board[3] == board[6]) {
+
+          if (board[0]===("x")) {
+              return +5;
+          }
+
+          if (board[0]===("o")) {
+              return -5;
+          }
+      }
+
+
+      if (board[1] == board[4] && board[4] == board[7]) {
+
+        if (board[1]===("x")) {
+            return +5;
+        }
+
+        if (board[1]===("o")) {
+            return -5;
+        }
+    }
+
+
+    if (board[2] == board[5] && board[5] == board[8]) {
+
+      if (board[2]===("x")) {
+          return +5;
+      }
+
+      if (board[2]===("o")) {
+          return -5;
+      }
+  }
+
+
+    if (board[0] == board[4] && board[4] == board[8]) {
+
+        if (board[0]===("x")) {
+            return +5;
+        }
+
+        if (board[0]===("o")) {
+            return -5;
+        }
+    }
+
+    if (board[2] == board[4] && board[4] == board[6]) {
+
+        if (board[2]===("x")) {
+            return +5;
+        }
+
+        if (board[2]===("o")) {
+            return -5;
+        }
+    }
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+const movesRemaining = function(board) {
+
+    for(let i = 0; i < 9; i++) {
+            if (board[i] == "") {
+                return true;
+            }
+    }
+
+    return false;
+
+}
+
+
+
+
+const miniMax = function(board, depth, isMaximizer) {
+
+  let outcome = heuristic_function(board);
+  if (outcome === 5) {
+      return outcome;
+  }
+
+  if (outcome === -5) {
+      return outcome;
+  }
+
+  if (movesRemaining(board) === false) {
+      return 0;
+  }
+
+  if (isMaximizer === true) {
+
+      let bestValue = -10000000;
+
+      for (let i = 0; i < 9; i++) {
+
+              if (board[i] === "") {
+
+                  board[i] = "x";
+
+                  bestValue = Math.max(bestValue, miniMax(board, depth + 1, !isMaximizer));
+
+                  board[i] = "";
+
+              }
+          
+      }
+
+      return bestValue;
+
+  } else {
+
+      let bestValue = 1000;
+
+      for (let j = 0; j < 9; j++) {
+
+              if (board[j]==="") {
+
+                  board[j] = "o";
+
+                  bestValue = Math.min(bestValue, miniMax(board, depth + 1, !isMaximizer));
+
+                  board[j] = "";
+
+              }
+          
+      }
+      return bestValue;
+
+  }
+
+}
+
+
+
+
+
+const optimalMove = function(board) {
+
+  let bestValue = -10000;
+
+  let coords = -1;
+
+  for (let i = 0; i < 9; i++) {
+
+          if (board[i] === "") {
+              board[i] = "x";
+
+              let value = miniMax(board, 0, false);
+            console.log("minimax" + value);
+              board[i] = "";
+
+              if (value > bestValue) {
+                  coords = i;
+                  bestValue = value;
+
+              }
+
+          }
+      }
+  
+  return coords;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   const buttons = document.querySelectorAll(".grid button");
   for (let button of buttons) {
     button.addEventListener("click", function () {
       if (validSpot(button)) {
-        switchTurn();
-        currentPlayer.move(button.getAttribute("data-num"));
-        winCondition();
-        turn++;
+        o_player.move(button.getAttribute("data-num"));
+        x_player.move(optimalMove(gameBoard.getBoard()));
+
       }
     });
   }
+
+return {movesRemaining, heuristic_function, optimalMove};
+
 })();
